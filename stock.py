c5364e230187ea8c5ccd5b8cf90fe4888c5ece31
BryanErickson <spyro3277@gmail.com>
2024-03-26 13:43:58 -0500

    CSCI3-7 Improved the Trader class

--- stock.py
+++ stock.py
@@ -1,10 +1,13 @@
 class stock:
-    def __init__(self, name, price):
+    def __init__(self, name, price, amt_of_shares):
         self.name = name
         self.price = price
+        self.amt_of_shares = amt_of_shares
     
     def getName(self):
         return self.name
     
     def getPrice(self):
         return self.price
+    def getShares(self):
+        return self.amt_of_shares
--- trader.py
+++ trader.py
@@ -7,14 +7,36 @@ class trader:
         currentMoney = 0.0
     
     def buyStock(self):
-        self.stockList.append(stock)
+        #Lets you add a stock to the surrent list, this input system will likely be changed later
+        print("Input stock name:")
+        stockName = input()
+        print("Input stock price: ")
+        stockPrice = input()
+        print("Input number of shares: ")
+        shares = input()
+
+        newStock = stock(stockName, stockPrice, shares)
+        self.stockList.append(newStock)
 
     def sellStock(self):
         #get stock name
-        print("Enter the stock name to sell:")
+        print("Enter the stock name, price, and shares to sell:")
         stockName = input()
 
         #Check if stock is on the list
+        for x in self.stockList:
+            if self.stockList == x:
+                #Get the price of the stock
+                #add to money
+                currentMoney += self.stockList[x].getPrice
+                self.stockList.remove(x)
+            else:
+                print("Not found")
+
+                
+            
+            
+            
         
         
         
