74b58c56628b680e680ddf4087b0e88cff28c703
NickBScsu <160097067+NickBScsu@users.noreply.github.com>
2024-04-17 10:50:56 +0000

    still need to modify to use yfinance module

--- Order.py
+++ Order.py
@@ -1,6 +1,11 @@
-from datetime import datetime, timedelta
-import FirstTry.StockInfo as si
-import yfinance as yf
+from datetime import datetime
+import yfinance as yf #not really used but i wish to when the other classes has been forged
+from abc import ABC, abstractmethod
+from typing import List, Tuple
+
+
+#TODO:
+# modify classes to use the yfinance module
 
 class Order:
     def __init__(self, id : int, price : float, quantity : int):
@@ -10,6 +15,7 @@ class Order:
         
         # this probably could be something else
         # maybe isocalender()
+        # sets time stamp of order to current date
         self.timestamp = datetime.today()
 
         self.order_type = None
@@ -32,17 +38,60 @@ class Order:
     def set_order_type(self, new_order_type: str):
         self.order_type = new_order_type
 
+    #reduces quantity of order by amount
     def reduce_quantity(self, amount: int):
-        return 0
+        if(amount > 0 and amount <= self.quantity):
+            self.quantity -= amount
+            print("Quantity has been reduced by {amount}")
+        else:
+            return "Amount out of range."
 
-# TODO:
-# implement MarketOrder, LimitOrder, OrderFactory, etc
-'''
+# MarketOrder and LimitOrder just inherit the Order class,
+# but the order type is their respective type (lol)
 class MarketOrder(Order):
-    def __init__():
-        return "what"
+    def __init__(self, trader_id, price, quantity):
+        super().__init__(trader_id, price, quantity)
+        self.order_type = "Market Order"
+    def get_order_type(self) -> str:
+        return super().get_order_type()
 
 class LimitOrder(Order):
-    def __init__():
-        return "what"
-'''
+    def __init__(self, trader_id, price, quantity):
+        super().__init__(trader_id, price, quantity)
+        self.order_type = "Limit Order"
+    def get_order_type(self) -> str:
+        return super().get_order_type()
+
+class OrderFactory(ABC):
+    @abstractmethod
+    def create_order(self, trader_id: int, price: float, quantity: int):
+        pass
+
+class MarketOrderFactory(OrderFactory):
+    def create_order(self, trader_id: int, price: float, quantity: int):
+        return MarketOrder(trader_id, price, quantity)
+
+class LimitOrderFactory(OrderFactory):
+    def create_order(self, trader_id: int, price: float, quantity: int):
+        return LimitOrder(trader_id, price, quantity)
+
+class OrderMatchingStrategy(ABC):
+    @abstractmethod
+    def match_orders(self, buy_orders: List[Order], sell_orders: List[Order]) -> Tuple[List[Order], List[Order]]:
+        pass
+
+# this sometimes causes errors in my ide, not an issue of my code
+# as i code perfectly every single time, its an issue with the typing
+# module i am using for this function
+class PriceTimeOrderMatchingStrategy(OrderMatchingStrategy):
+    def match_orders(self, buy_orders: List['Order'], sell_orders: List['Order']) -> Tuple[List['Order'], List['Order']]:
+        matched_orders: List['Order'] = []
+
+        for sell_order in sell_orders:
+            for buy_order in buy_orders[:]:
+                if sell_order.get_price() <= buy_order.get_price():
+                    matched_orders.append(buy_order)
+                    buy_orders.remove(buy_order)
+
+        buy_orders.extend(matched_orders)
+        return buy_orders, sell_orders
