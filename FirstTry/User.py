1ae9211f86818e6bd9d8fb223d0a8d8ee7822025
gabrielsong9674 <gracegabrielson@icloud.com>
2024-04-26 12:47:27 -0500

    show buy summary on ui

--- FirstTry/User.py
+++ FirstTry/User.py
@@ -5,9 +5,13 @@ import yfinance as yf
 import plotly.graph_objects as go
 from datetime import datetime, timedelta
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QTableWidget, QTableWidgetItem, QHBoxLayout, QVBoxLayout, QLabel, QMessageBox, QInputDialog
+from PyQt5.QtCore import QObject, pyqtSignal
 from concurrent.futures import ThreadPoolExecutor
 from trader import *
+
+
 class StockVisualizer(QWidget):
+    summary = ""
     def __init__(self):
         super().__init__()
         self.init_ui()
@@ -22,13 +26,11 @@ class StockVisualizer(QWidget):
         self.amount = QLabel("Amount: ")
         self.stockAmountInput = QLineEdit()
         self.buyStockButton = QPushButton("Buy")
-        self.sellStockButton = QPushButton("Sell")
+        self.buyStockLabel = QLabel("Summary of Costs")
         self.getDataButton.clicked.connect(self.get_stock_data) 
         self.buyStockButton.clicked.connect(self.buy_multiple)
-        
-
         self.dataTable = QTableWidget()
-
+    
         layout = QVBoxLayout()
         layout.addWidget(ticker_label)
         layout.addWidget(self.tickerInput)
@@ -38,7 +40,7 @@ class StockVisualizer(QWidget):
         layout.addWidget(self.amount)
         layout.addWidget(self.stockAmountInput)
         layout.addWidget(self.buyStockButton)
-        layout.addWidget(self.sellStockButton)
+        layout.addWidget(self.buyStockLabel)
         layout.addWidget(self.dataTable)
 
         self.setLayout(layout)
@@ -80,11 +82,13 @@ class StockVisualizer(QWidget):
         fig.show()
 
     def buy_one_stock(self, s, a):
+
         stock = yf.Ticker(s)
         closing = stock.history(period='1D')['Close'].iloc[0]
-        cost = (float(closing) *float(a))   
-        print("Cost: "+ str(cost))
+        cost = (float(closing) *float(a)) 
+        self.summary += "{} shares of {} will cost ${:0.2f}\n".format(a, s, cost) 
     def buy_multiple(self):
+        self.summary = ""
         stocksToBuy = self.stockNameInput.text().split(" ")
         amounts = self.stockAmountInput.text().split(" ")
         stocks_and_amounts = {stock: amount for stock, amount in zip(stocksToBuy, amounts)}
@@ -99,8 +103,9 @@ class StockVisualizer(QWidget):
         for t in threads:
             t.join()
         
+       
 
-
+        self.buyStockLabel.setText(self.summary)
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     visualizer = StockVisualizer()
